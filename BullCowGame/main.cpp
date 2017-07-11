4e7931cf0493117fcd3f256734e7b54ae88bb13b
Volodymyr Lipunov <volo_lipu@yahoo.com>
2017-07-10 17:37:29 -0700

    Initial Commit

---
+++ BullCowGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BullCowGame
+//
+//  Created by Volodymyr Lipunov on 7/10/17.
+//  Copyright © 2017 Volodymyr Lipunov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
